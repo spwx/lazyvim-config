bbfb59955255dcd0327c2f6d1fac66b6cc76263a
Sean <sean@wall.ninja>
2024-09-22 22:59:33 -0400

    fix: new keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true })
+vim.keymap.set({ "n", "v" }, "gh", "^", { silent = true })
+vim.keymap.set({ "n", "v" }, "gl", "$", { silent = true })
